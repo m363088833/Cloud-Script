d831078f19456067cbe3c3cafb50cf8757a4144c
m363088833 <118506674+m363088833@users.noreply.github.com>
2022-12-05 04:25:46 +0800

    云端脚本主程序
    在线修改文档即可实现（软件更新）

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ ui.layout(
 
 let storage = storages.create("云端脚本");
 storage.put("手机默认文件夹路径", "/storage/emulated/0/脚本/云端脚本/");
-更新_js文件("https://raw.githubusercontent.com/m363088833/Cloud-Script/main/main.js","./main.js");
+更新_js文件("https://raw.githubusercontent.com/m363088833/Cloud-Script/main/main.js", "./main.js");
 更新_js文件("https://raw.githubusercontent.com/m363088833/Cloud-Script/main/__library__.js");
 
 ui.ok.click(() => {
@@ -37,6 +37,7 @@ ui.ok.click(() => {
             let path = files.join(storage.get("手机默认文件夹路径"), fileName);
             files.ensureDir(path); //确保路径中的文件夹存在
             files.write(path, data); //保存文件
+            storage.put("上次历史脚本", path);
             toastLog("存储路径：" + path);
             engines.execScriptFile(path); //启动脚本
         });
@@ -78,10 +79,10 @@ function 更新_js文件(url, path) {
     let Thread1 = threads.start(function() {
         let fileName = 获取github云端脚本文件名(url); //获取文件名
         let data = http.get(url).body.string(); //http请求
-        if (path == undefined) {
-            var path = files.join(storage.get("手机默认文件夹路径"), fileName);
-alert(path);
+        if (storage.get(fileName) == undefined) {
+            storage.put(fileName,files.join(storage.get("手机默认文件夹路径"), fileName));
         }
+        let path = storage.get(fileName);
         files.ensureDir(path); //确保路径中的文件夹存在
         files.write(path, data); //保存文件
         toastLog(fileName + "已更新：" + path);
